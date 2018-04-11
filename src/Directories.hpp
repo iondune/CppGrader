9314c36ab5109856f5fee855108ca1e2516eb864
Ian Dunn <iondune@gmail.com>
2018-04-10 23:54:42 -0700

    Keep template files in repo

--- src/Directories.hpp
+++ src/Directories.hpp
@@ -7,7 +7,7 @@
 string const ExecDirectory = "/home/ian/";
 string const AllStudentsDirectory = "/home/ian/students/";
 string const SiteDirectory = "/var/www/html/grades/";
-string const TemplateDirectory = "/home/ian/csc473-gradeserver/html/";
+string const TemplateDirectory = "/home/ian/CppGrader/template/";
 string const AllTestsDirectory = "/home/ian/csc473-testfiles/";
 string const InputsDirectory = "/home/ian/csc473-inputfiles/";
 // string const StudentHTMLDirectory="${site_directory}/${student}/${assignment}/"
